daa825881eea265a30dd838aa9dffcfca9e2b43f
sandipan-Coder <palsandipan68@gmail.com>
2025-11-03 20:17:27 +0530

    This code help to analyse traffic in my web

---
+++ Analytics.js
@@ -0,0 +1,12 @@
+
+//* This line pest in the index.html file in react app to analysis the user in my app.
+
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-0P7TD577M2"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-0P7TD577M2');
+// </script>
